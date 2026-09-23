be0bcdc4e51cf878d8428c999e79a1fc933ff825
shs0088 <eng.shakhsheer@gmail.com>
2026-09-23 23:26:13 +0300

    M1: add portable Odoo addon foundation

---
+++ odoo19_migration/odoo/custom_addons/dtf_sale/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "DTF Studio dtf_sale",
+    "version": "19.0.1.0.0",
+    "summary": "M1 portable foundation skeleton",
+    "license": "LGPL-3",
+    "depends": [
+        "dtf_core",
+        "sale_management",
+        "stock",
+        "dtf_design",
+        "dtf_customizer"
+    ],
+    "data": [],
+    "installable": true,
+    "application": false
+}
