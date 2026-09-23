1bea52b11e706dd8551601fac4dc7d9ba6c0564f
shs0088 <eng.shakhsheer@gmail.com>
2026-09-24 02:25:14 +0300

    M5: wire native checkout and production models

--- odoo19_migration/odoo/custom_addons/dtf_sale/__manifest__.py
+++ odoo19_migration/odoo/custom_addons/dtf_sale/__manifest__.py
@@ -1,16 +1,10 @@
 {
-    "name": "DTF Studio dtf_sale",
-    "version": "19.0.1.0.0",
-    "summary": "M1 portable foundation skeleton",
+    "name": "DTF Studio Sales and Checkout",
+    "version": "19.0.2.0.0",
+    "summary": "Native Odoo sales checkout snapshots and 30-minute stock reservations",
     "license": "LGPL-3",
-    "depends": [
-        "dtf_core",
-        "sale_management",
-        "stock",
-        "dtf_design",
-        "dtf_customizer"
-    ],
-    "data": [],
-    "installable": True,
-    "application": False
+    "depends": ["dtf_core", "sale_management", "stock", "dtf_design", "dtf_preflight"],
+    "data": ["security/ir.model.access.csv"],
+    "installable": true,
+    "application": false
 }
