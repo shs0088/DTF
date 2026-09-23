4d555d441121d2cd8dc156c98d17e507afa9055e
shs0088 <eng.shakhsheer@gmail.com>
2026-09-23 22:57:07 +0300

    odoo19 prep: add odoo19_migration/odoo/custom_addons/dtf_api/__manifest__.py

---
+++ odoo19_migration/odoo/custom_addons/dtf_api/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "DTF Studio API",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["base", "product", "sale_management", "stock", "website_sale"],
+    "installable": True,
+    "application": False,
+}
