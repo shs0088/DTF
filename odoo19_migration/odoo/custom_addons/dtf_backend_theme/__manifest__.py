41ac4230eecb8066c1ee8b48a3ce35f08c3379e7
shs0088 <eng.shakhsheer@gmail.com>
2026-09-23 23:26:29 +0300

    M1: add portable Odoo addon foundation

---
+++ odoo19_migration/odoo/custom_addons/dtf_backend_theme/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "DTF Studio dtf_backend_theme",
+    "version": "19.0.1.0.0",
+    "summary": "M1 portable foundation skeleton",
+    "license": "LGPL-3",
+    "depends": [
+        "web"
+    ],
+    "data": [],
+    "installable": true,
+    "application": false
+}
