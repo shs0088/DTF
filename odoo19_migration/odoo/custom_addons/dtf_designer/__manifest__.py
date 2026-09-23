2084b7e8e211788a6387b2595d2ed17d927a6d29
shs0088 <eng.shakhsheer@gmail.com>
2026-09-23 23:48:57 +0300

    M2: enable designer model and security

--- odoo19_migration/odoo/custom_addons/dtf_designer/__manifest__.py
+++ odoo19_migration/odoo/custom_addons/dtf_designer/__manifest__.py
@@ -1,12 +1,16 @@
 {
-    "name": "DTF Studio dtf_designer",
-    "version": "19.0.1.0.0",
-    "summary": "M1 portable foundation skeleton",
+    "name": "DTF Studio Designer",
+    "version": "19.0.2.0.0",
+    "summary": "DTF Studio designer identity and qualification foundation",
+    "category": "DTF Studio",
     "license": "LGPL-3",
     "depends": [
         "dtf_core"
     ],
-    "data": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/designer_rules.xml"
+    ],
     "installable": True,
     "application": False
 }
