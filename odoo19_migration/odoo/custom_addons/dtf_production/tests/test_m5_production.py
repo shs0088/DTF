09ff6f449af3030fe2b4997f8a003062d0c770c4
shs0088 <eng.shakhsheer@gmail.com>
2026-09-24 09:54:23 +0300

    M5 native reconciliation: remove deferred handoff test

--- odoo19_migration/odoo/custom_addons/dtf_production/tests/test_m5_production.py
+++
@@ -1,9 +0,0 @@
-from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
-
-
-class TestDTFM5Production(TransactionCase):
-    def test_handoff_requires_confirmed_checkout_and_snapshots(self):
-        self.assertTrue(hasattr(self.env["dtf.production.handoff"], "create_from_line"))
-        self.assertTrue(hasattr(self.env["sale.order.line"], "dtf_preflight_snapshot"))
-        self.assertTrue(hasattr(self.env["sale.order.line"], "dtf_master_asset_id"))
